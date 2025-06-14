de308e21ac607cdeb1fbfec6c0415a139e678a6f
github-ci <you@example.com>
2025-06-15 00:18:40 +0800

    init

---
+++ README.zh-CN.md
@@ -0,0 +1,25 @@
+# doctrine-dedicated-entity-manager-bundle
+
+xxx
+
+## 安装
+
+```bash
+composer require tourze/doctrine-dedicated-entity-manager-bundle
+```
+
+## 使用方法
+
+待补充
+
+## 配置
+
+待补充
+
+## 示例
+
+待补充
+
+## 参考文档
+
+- [示例链接](https://example.com)
---
+++ src/DoctrineDedicatedEntityManagerBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tourze\DoctrineDedicatedEntityManagerBundle;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
+
+class DoctrineDedicatedEntityManagerBundle extends Bundle implements BundleDependencyInterface
+{
+    public static function getBundleDependencies(): array
+    {
+        return [
+            DoctrineBundle::class => ['all' => true],
+        ];
+    }
+}
